58c25f14e1dd76aa6cba63474bf77cd6c1d953d4
tylerl0706 <tylerl0706@gmail.com>
2015-01-09 22:51:51 -0500

    Updated cordova splash screen and icon for Android

---
+++ mobile-config.js
@@ -0,0 +1,26 @@
+App.info({
+  id: 'com.tylerleonhardt.jott',
+  name: 'Jott',
+  description: 'A lightweight, mobile friendly note taking service geared towards copying and pasting between devices. Made using the Meteor.js framework.',
+  version: '1.0.0',
+  author: 'Tyler Leonhardt',
+  email: 'ihardt.dev@gmail.com',
+  website: 'https://jott.meteor.com'
+});
+
+App.icons({
+  'android_mdpi': 'resources/icons/mipmap-mdpi/ic_launcher.png',
+  'android_hdpi': 'resources/icons/mipmap-hdpi/ic_launcher.png',
+  'android_xhdpi': 'resources/icons/mipmap-xhdpi/ic_launcher.png',
+});
+
+App.launchScreens({
+  'android_ldpi_portrait': 'resources/splash/res-long-port-ldpi/default.png',
+  'android_ldpi_landscape': 'resources/splash/res-long-land-ldpi/default.png',
+  'android_mdpi_portrait': 'resources/splash/res-long-port-mdpi/default.png',
+  'android_mdpi_landscape': 'resources/splash/res-long-land-mdpi/default.png',
+  'android_hdpi_portrait': 'resources/splash/res-long-port-hdpi/default.png',
+  'android_hdpi_landscape': 'resources/splash/res-long-land-hdpi/default.png',
+  'android_xhdpi_portrait': 'resources/splash/res-long-port-xhdpi/default.png',
+  'android_xhdpi_landscape': 'resources/splash/res-long-land-xhdpi/default.png'
+});
